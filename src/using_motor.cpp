f84bb30c2458c38b7ed44992de52ff575518a8c3
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2021-06-09 15:26:37 +0300

    add motor lib - done for first release

--- src/using_motor.cpp
+++ src/using_motor.cpp
@@ -6,18 +6,12 @@
 #endif
  */
 #include "using_motor.h"
+//#include <filters/alfaBeta.h>
 #pragma once
 
-//void motor::motor() {
-//  
-//}
-
-// #include <filters/alfaBeta.h>
-
-void Motor::SetMode(String a, int a_pin, String b="", int b_pin=0,  String c="", int c_pin=0) { // задаем пины для работы с шилдом мотора
+motor::motor(String a, int a_pin, String b="", int b_pin=0,  String c="", int c_pin=0) {
   char dir = 0, pwm = 0;
 
-  // проверка на корректность ввода
   if      (a=="DIR") dir++;
   else if (a=="PWM") pwm++;
   else dir-=10;
@@ -28,21 +22,19 @@ void Motor::SetMode(String a, int a_pin, String b="", int b_pin=0,  String c="",
   else if (c=="PWM") pwm++;
   else if (c!="")  dir-=10;
   
-  if (dir>0) { // если нет ошибок в вводе, задаем режим работы
+  if (dir>0) {
     if      (dir==1 && pwm==0) _mode = 1;
     else if (dir==2 && pwm==0) _mode = 2;
     else if (dir==1 && pwm==1) _mode = 3;
     else if (dir==2 && pwm==1) _mode = 4;
-    else _mode = 0; // такого режима нет
+    else _mode = 0; 
   }
   
-  if (_mode>0) { // такой режим работы существует
-    // обнуляем переменные
+  if (_mode>0) { 
     _pwm_pin = 0;
     _dir_1_pin = 0;
     _dir_2_pin = 0;
 
-    // задаем им новые значения
     if      (a=="DIR") _dir_1_pin = a_pin;
     else if (a=="PWM") _pwm_pin = a_pin;
     else dir-=10;
@@ -53,36 +45,35 @@ void Motor::SetMode(String a, int a_pin, String b="", int b_pin=0,  String c="",
     else if (c=="PWM") _pwm_pin = c_pin;
     else dir-=10;
 
-    // запускаем их
     pinMode(_dir_1_pin, OUTPUT);
     if (_dir_2_pin>0) pinMode(_dir_2_pin, OUTPUT);
   }
-  else { // вывод ошибки (неправильно задан мотор)
+  else {
     _mode = 0;
     Serial.println("motor SetMode - ERROR!!!");
   }
 
   // константы
-  _max_speed = 100; // значение скорости по умолчанию
+  _max_speed = 100;
 }
 
-void Motor::SetSpeed(int predel) { // настраиваем новые значения, которые будут масштабироваться в 255 на мотор
+void motor::SetSpeed(int predel) {
   if (predel<1) Serial.println("ERROR: <SetSpeed> - invalid value");
   else _max_speed = predel; 
 } 
 
-int Motor::TestMode() { return _mode; } // проверяем режим работы
+int motor::TestMode() { return _mode; } 
 
-void Motor::run(int speed) {
+void motor::run(int speed) {
   if (_mode==0) return;
   if (_mode==3 || _mode==4) analogWrite(_pwm_pin, map(constrain(abs(speed),0,_max_speed),0,_max_speed,0,255));
   digitalWrite(_dir_1_pin,speed>0);
   if (_mode==2 || _mode==4) digitalWrite(_dir_2_pin,speed<0);
 }
 
-void Motor::stop() { Motor::run(0); }
+void motor::stop() { motor::run(0); }
 
-void Motor::block() {
+void motor::block() {
   if (_mode==3 || _mode==4) analogWrite(_pwm_pin, 0);
   if (_mode==2 || _mode==4) {
     digitalWrite(_dir_1_pin,1);
@@ -92,10 +83,3 @@ void Motor::block() {
     digitalWrite(_dir_1_pin,0);
   }
 }
-
-/*float Motor::version(float a=0) {
-   if (a==0) return motor_lib_version;
-   if (a==motor_lib_version) return 1;
-   Serial.println("WARNING: lib <arduino_motor> have more version!");
-   return 0;
-}*/
--- src/using_motor.h
+++ src/using_motor.h
@@ -2,6 +2,7 @@
 #pragma once
 
 /*
+mode:
 0 - ERROR
 1 - DIR
 2 - DIR, DIR
@@ -9,30 +10,22 @@
 4 - SPEED, DIR, DIR
 */
 
-//#define motor_lib_version  1.0
+#define DIR "DIR"
+#define PWM "PWM"
 
-//#include "arduino_motor.h"
-//Motor test_motor;
-  //test_motor.SetMode("DIR",3, "DIR",4);
-  //Serial.println(test_motor.TestMode());
-  //test_motor.SetSpeed(103);
-
-class Motor {
+class motor {
 
   public:
 
     //#define AUTOMATIC  1
 
-    //motor();
-    void SetMode(String a, int a_pin, String b="", int b_pin=0,  String c="", int c_pin=0);
+    motor(String a, int a_pin, String b="", int b_pin=0,  String c="", int c_pin=0);
     int TestMode();
     void SetSpeed(int predel);
 
     void run(int speed);
     void stop();
     void block();
-
-    //float version(float a=0);
 
   private:
 
